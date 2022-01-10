ef73272acae6679bc23edcc31a29b343bfb5627b
Evandro <evandrosilva558@hotmail.com>
2022-01-10 16:15:55 -0300

    imprimindo uma lista utilizando foreach e lambda

---
+++ src/dio/streamApi/ExerciciosStreamApi.java
@@ -0,0 +1,20 @@
+package dio.streamApi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExerciciosStreamApi {
+
+	public static void main(String[] args) {
+		
+		// lista de elementos
+		List<String> numerosAleatorios = Arrays.asList("1", "2", "3", "4", "5");
+		
+		System.out.println("Imprima todos od elementos dessa lista de String;");
+		
+		// funcao de impressao utilizando forEach e interface funcional lambida.
+		numerosAleatorios.forEach(s -> System.out.println(s));
+
+	}
+
+}
